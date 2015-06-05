53f5327ea611972d994af5122028af483faf27f7
Christian Brückner <family.brueckner@googlemail.com>
2015-06-05 20:41:06 +0200

    improved navigation.js, completely minified theme.min.js

--- assets/js/src/theme/navigation.js
+++ assets/js/src/theme/navigation.js
@@ -1,6 +1,5 @@
 // Navigation  _________________________________________
 // responsive navigation
-//
 (function (doc) {
 	"use strict"; // jshint ~_~
 
@@ -11,46 +10,48 @@
 			navigationIsVisible   : 'chrico-navigation--is-visible',
 			toggleIsActive        : 'chrico-navigation__toggle--is-active'
 		},
-		$toggle = doc.getElementById('chrico-navigation__toggle'),
+		$toggleButton = doc.getElementById('chrico-navigation__toggle'),
 		$menuItem = doc.getElementsByClassName(classes.menuItem),
 		$body = doc.body;
 
 	// initialize all events.
 	Navigation.initialize = function () {
-		Navigation.registerToggleEvent();
-		Navigation.registerHoverEvent();
+		Navigation.registerToggleEvents();
+		Navigation.registerHoverEvents();
+	};
+
+	// contains all event handler for our Navigation.
+	Navigation.HANDLER = {
+
+		// onClick the toggle Button to show/hide the navigation.
+		toggleNavigationVisibility: function (e) {
+			e.preventDefault();
+			$toggleButton.classList.toggle(classes.toggleIsActive);
+			$body.classList.toggle(classes.navigationIsVisible);
+		},
+
+		// onMouseleave show the sub-navigation.
+		showSubNavigation            : function () {
+			this.classList.add(classes.subNavigationIsVisible);
+		},
+
+		// onMouseenter hide the sub-navigation.
+		hideSubNavigation            : function () {
+			this.classList.remove(classes.subNavigationIsVisible);
+		}
+
 	};
 
 	// register the event on toggle-Button for mobile/tablet view to show/hide the Navigation.
-	Navigation.registerToggleEvent = function () {
-		bind(
-			$toggle,
-			'click',
-			function (e) {
-				e.preventDefault();
-				$toggle.classList.toggle(classes.toggleIsActive);
-				$body.classList.toggle(classes.navigationIsVisible);
-			}
-		);
+	Navigation.registerToggleEvents = function () {
+		bind($toggleButton, 'click', Navigation.HANDLER.toggleNavigationVisibility);
 	};
 
-	// register the mouseEnter/-Leave-Event for show/hide the Sub-Navigation.
-	Navigation.registerHoverEvent = function () {
+	// register the mouseEnter/-Leave-HANDLER for show/hide the Sub-Navigation.
+	Navigation.registerHoverEvents = function () {
 		[].forEach.call($menuItem, function ($element) {
-			bind(
-				$element,
-				'mouseenter',
-				function () {
-					this.classList.add(classes.subNavigationIsVisible);
-				}
-			);
-			bind(
-				$element,
-				'mouseleave',
-				function () {
-					this.classList.remove(classes.subNavigationIsVisible);
-				}
-			)
+			bind($element, 'mouseenter', Navigation.HANDLER.showSubNavigation);
+			bind($element, 'mouseleave', Navigation.HANDLER.hideSubNavigation)
 		});
 	};
 
