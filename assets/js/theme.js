0473b5ed3f4cf27f83ab2c2a29c50e44c266fd41
Christian Brückner <family.brueckner@googlemail.com>
2015-06-08 20:09:36 +0200

    updated baguette.js, fixed word-break bug, fixed gallery bug on mobile, fixed font loading

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -2,11 +2,11 @@
 // requires addon/baguette.js
 //
 (function () {
-	"use strict"; // jshint ~_~
+//	"use strict"; // jshint ~_~
 
-	baguetteBox.run('.chrico-gallery', {
+	baguetteBox.run('.chrico-gallery'/*, {
 		animation: 'fadeIn'
-	});
+	}*/);
 
 })();
 
